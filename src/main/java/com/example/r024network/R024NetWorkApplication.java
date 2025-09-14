d6ec4129ca0e67a8c9b90503448cb2ccac3ed9fd
ATC_rock <11522154+atc-rock@user.noreply.gitee.com>
2025-09-14 22:16:48 +0800

    rawProj

---
+++ src/main/java/com/example/r024network/R024NetWorkApplication.java
@@ -0,0 +1,13 @@
+package com.example.r024network;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class R024NetWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(R024NetWorkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/r024network/R024NetWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.r024network;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class R024NetWorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
